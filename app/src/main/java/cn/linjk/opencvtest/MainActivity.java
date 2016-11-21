43034fea213579d24c63f421b8f6a66fa1393a53
LinJK <linjk121@163.com>
2016-11-21 16:13:29 +0800

    integrate opencv functions success.

--- app/src/main/java/cn/linjk/opencvtest/MainActivity.java
+++ app/src/main/java/cn/linjk/opencvtest/MainActivity.java
@@ -3,11 +3,15 @@ package cn.linjk.opencvtest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import cn.linjk.jniBridge.OpenCVUtils;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        OpenCVUtils.img2gray("");
     }
 }
--- app/src/main/jni/cn_linjk_jniBridge_OpenCVUtils.cpp
+++ app/src/main/jni/cn_linjk_jniBridge_OpenCVUtils.cpp
@@ -4,3 +4,27 @@
 
 #include "cn_linjk_jniBridge_OpenCVUtils.h"
 
+#include <opencv2/opencv.hpp>
+
+#include <Android/log.h>
+#include <Android/asset_manager.h>
+#include <Android/asset_manager_jni.h>
+
+#define TAG "cn.linjk.opencvtest.jni"
+#define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG,TAG ,__VA_ARGS__)
+#define LOGI(...) __android_log_print(ANDROID_LOG_INFO,TAG ,__VA_ARGS__)
+#define LOGW(...) __android_log_print(ANDROID_LOG_WARN,TAG ,__VA_ARGS__)
+#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR,TAG ,__VA_ARGS__)
+#define LOGF(...) __android_log_print(ANDROID_LOG_FATAL,TAG ,__VA_ARGS__)
+
+extern "C" {
+
+JNIEXPORT void JNICALL Java_cn_linjk_jniBridge_OpenCVUtils_img2gray
+    (JNIEnv *env , jclass objClass, jstring imgFilePath) {
+
+        LOGI("OpenCV version: %s", CV_VERSION);
+
+    }
+
+}
+
